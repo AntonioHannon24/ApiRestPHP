28c869c27850c8a9ab40924b67d1f4785b014d6b
Antonio <antoniohannon24@gmail.com>
2023-01-04 17:28:39 -0300

    confirmar

--- www/public/index.php
+++ www/public/index.php
@@ -2,23 +2,29 @@
 require "../vendor/autoload.php";
 require "../routes/router.php";
 
+$api=explode("/",$_SERVER["REQUEST_URI"]);
 
-try{
-    $uri=parse_url($_SERVER["REQUEST_URI"])['path']; // uri da url
-    $request=$_SERVER["REQUEST_METHOD"]; // metodo da requisição
-   
-    if(! isset($router[$request])){ // Verifica se não existe o metodo no array router dentro de router.php
-        echo "O metodo da requisição não existe!!";
+if($api[1] == "api"){
+    
+    try{
+        $uri=parse_url($_SERVER["REQUEST_URI"])['path']; // uri da url
+        
+        $request=$_SERVER["REQUEST_METHOD"]; // metodo da requisição
+       
+        if(! isset($router[$request])){ // Verifica se não existe o metodo no array router dentro de router.php
+            echo "O metodo da requisição não existe!!";
+        }
+        if(! array_key_exists($uri,$router[$request])){// verifica se não existe a rota/requisição
+            echo "Erro em enviar/solicitar a requisição!!";
+        }
+    
+        // se passar das verificações, 
+       $controller = $router[$request][$uri]; // recebe os dados
+       $controller(); 
+    }catch(Exception $e){
+        $e->getMessage();
     }
-    if(! array_key_exists($uri,$router[$request])){// verifica se não existe a rota/requisição
-        echo "Erro em enviar/solicitar a requisição!!";
-    }
-
-    // se passar das verificações, 
-   $controller = $router[$request][$uri]; // recebe os dados
-   $controller(); 
-}catch(Exception $e){
-    $e->getMessage();
+}else{
+    echo "Caminho para a API Incorreto!!";
 }
-
 ?>
