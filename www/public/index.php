5d39e846685d95c508449217dcb286a9eecf1faa
Antonio <antoniohannon24@gmail.com>
2023-01-04 17:18:35 -0300

    confirmar

--- www/public/index.php
+++ www/public/index.php
@@ -4,32 +4,21 @@ require "../routes/router.php";
 
 
 try{
-
     $uri=parse_url($_SERVER["REQUEST_URI"])['path']; // uri da url
-
     $request=$_SERVER["REQUEST_METHOD"]; // metodo da requisição
    
     if(! isset($router[$request])){ // Verifica se não existe o metodo no array router dentro de router.php
         echo "O metodo da requisição não existe!!";
-
-        var_dump($router[$request]);
-       
     }
     if(! array_key_exists($uri,$router[$request])){// verifica se não existe a rota/requisição
-
         echo "Erro em enviar/solicitar a requisição!!";
-        var_dump($uri,$router[$request]);
     }
 
     // se passar das verificações, 
-
    $controller = $router[$request][$uri]; // recebe os dados
    $controller(); 
-  
 }catch(Exception $e){
     $e->getMessage();
 }
-
-
 
 ?>
